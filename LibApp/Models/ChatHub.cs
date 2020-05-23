ae1fce3a818b6af1a37cf88ae8739ce38f457d77
agajek777 <adam.gajek777@gmail.com>
2020-05-24 00:02:40 +0200

    Add EmailDomain Attribute

---
+++ LibApp/Models/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace LibApp.Models
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+
+        }
+    }
+}
---
+++ LibApp/Validation/ValidEmailDomainAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Validation
+{
+    public class ValidEmailDomainAttribute : ValidationAttribute
+    {
+        private readonly string _allowedDomain;
+
+        public ValidEmailDomainAttribute(string allowedDomain)
+        {
+            this._allowedDomain = allowedDomain;
+        }
+        public override bool IsValid(object value)
+        {
+            string[] strings = value.ToString().Split('@');
+            return strings[1].ToUpper() == _allowedDomain.ToUpper();
+
+        }
+    }
+}
