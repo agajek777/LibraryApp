5f23363255e47ceed2c1cc8f992c13938e493e08
agajek777 <adam.gajek777@gmail.com>
2020-05-28 19:57:52 +0200

    Add MessagesAPI

---
+++ LibApp/Controllers/API/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibApp.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MessagesController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        [HttpGet]
+        public async Task<JsonResult> GetMessagesAsync()
+        {
+            var msgs = await _db.Messages.OrderBy(m => m.Sent).Include(m => m.AppUser).ToListAsync();
+            return Json(new { data = msgs });
+        }
+    }
+}
